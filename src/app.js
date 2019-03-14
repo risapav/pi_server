91ffb592200c4032c6938b2e662ba704dc27b577
Pavol Risa <risapav@gmail.com>
2019-03-14 22:12:53 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ var {
   Telegram
 } = require('src/telegram');
 
-
+/*
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort(path, { baudRate: 115200 });
@@ -30,6 +30,7 @@ port.pipe(parser)
 
 parser.on('data', line => console.log(`> ${line}`))
 port.write('ROBOT POWER ON\n')
+*/
 //> ROBOT ONLINE
 /*
 //nastavenie prenosu po seriovej linke
