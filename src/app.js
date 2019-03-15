bfdfa8be19065a736a09afa346e6ef8fadedf3f6
Pavol Risa <risapav@gmail.com>
2019-03-15 16:01:05 +0100

    updated library

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //app.js
-//const serialjs = require('serialport-js');
 const server = require('server');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
 
 const {
   get,
@@ -20,90 +21,95 @@ var {
   Telegram
 } = require('src/telegram');
 
-/*
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
-const port = new SerialPort(path, { baudRate: 115200 });
+var comport;
 
-const parser = new Readline()
-port.pipe(parser)
-
-parser.on('data', line => console.log(`> ${line}`))
-port.write('ROBOT POWER ON\n')
-*/
-//> ROBOT ONLINE
-/*
 //nastavenie prenosu po seriovej linke
-const init = async () => {
+const init = () => {
+  console.log(process.platform);
+  if (process.platform === "win32") {
+    comport = 'COM7';
+  } else {
+    comport = '/dev/ttyACM0';
+  }
+  console.log('comport ', comport);
+
+  SerialPort.list().then(
+    ports => ports.forEach(console.log),
+    err => console.error(err)
+  );
+
+  const port = new SerialPort(comport, {
+    baudRate: 115200
+  }, (err) => {
+    if (err) {
+      return console.log('Error: ', err.message);
+    }
+  });
+
+  const parser = port.pipe(new Readline({
+    delimiter: '\n'
+  }));
+
+  // Open errors will be emitted as an error event
+  port.on('error', function(err) {
+    console.log('Error: ', err.message);
+  });
+
   console.log('init  >>>>>>>>>>');
   const interval = 1000; //1 sekunda
 
-  const delimiter = '\n';
-
-  const ports = serialjs.find();
-  console.log(ports);
-  if (ports.length) {
-    try {
-      let port = await serialjs.open(ports[0].port, delimiter);
-    } catch (e) {
-      console.log(e);
+  parser.on('data', (data) => {
+    console.log(data);
+    var msg = new Telegram;
+    msg.setBuffer(data);
+    msg.decodeTelegram();
+    if (msg.isValidTelegram()) {
+      console.log('is valid');
+      RX_Message.stx = msg.getByteInTelegram(CONSTANTS.START);
+      RX_Message.b_0 = msg.getUint16(0);
+      RX_Message.b_1 = msg.getUint16(1);
+      RX_Message.b_2 = msg.getUint16(2);
+      RX_Message.b_3 = msg.getUint16(3);
+      RX_Message.b_4 = msg.getUint16(4);
+      RX_Message.b_5 = msg.getUint16(5);
+      RX_Message.b_6 = msg.getUint16(6);
+      RX_Message.b_7 = msg.getUint16(7);
+      RX_Message.b_8 = msg.getUint16(8);
+      RX_Message.b_9 = msg.getUint16(9);
+      RX_Message.etx = msg.getByteInTelegram(CONSTANTS.STOP);
     }
-    port.on('data', (data) => {
-      console.log(data);
-      var msg = new Telegram;
-      msg.setBuffer(data);
-      msg.decodeTelegram();
-      if (msg.isValidTelegram()) {
-        console.log('is valid');
-        RX_Message.stx = msg.getByteInTelegram(CONSTANTS.START);
-        RX_Message.b_0 = msg.getUint16(0);
-        RX_Message.b_1 = msg.getUint16(1);
-        RX_Message.b_2 = msg.getUint16(2);
-        RX_Message.b_3 = msg.getUint16(3);
-        RX_Message.b_4 = msg.getUint16(4);
-        RX_Message.b_5 = msg.getUint16(5);
-        RX_Message.b_6 = msg.getUint16(6);
-        RX_Message.b_7 = msg.getUint16(7);
-        RX_Message.b_8 = msg.getUint16(8);
-        RX_Message.b_9 = msg.getUint16(9);
-        RX_Message.etx = msg.getByteInTelegram(CONSTANTS.STOP);
+  });
+
+  setInterval(() => {
+    var msg = new Telegram;
+    TX_Message.b_6++;
+
+    console.log('TX_Message', TX_Message.b_6);
+    msg.setByteInTelegram(CONSTANTS.START, CONSTANTS.STX);
+    msg.setUint16(0, TX_Message.b_0);
+    msg.setUint16(1, TX_Message.b_1);
+    msg.setUint16(2, TX_Message.b_2);
+    msg.setUint16(3, TX_Message.b_3);
+    msg.setUint16(4, TX_Message.b_4);
+    msg.setUint16(5, TX_Message.b_5);
+    msg.setUint16(6, TX_Message.b_6);
+    msg.setUint16(7, TX_Message.b_7);
+    msg.setUint16(8, TX_Message.b_8);
+    msg.setUint16(9, TX_Message.b_9);
+    msg.setByteInTelegram(CONSTANTS.STOP, CONSTANTS.ETX);
+    msg.encodeTelegram();
+    port.write(msg.getBuffer().toString('ascii') + '\n', (err) => {
+      if (err) {
+        return console.log('Error on write: ', err.message);
       }
-    });
+      console.log(msg.getBuffer().toString('ascii') + '\n', err, msg);
+    })
 
-    port.on('error', (error) => {
-      console.error(error);
-    });
-
-    setInterval(async () => {
-      try {
-        var msg = new Telegram;
-        TX_Message.b_6++;
-
-        console.log('TX_Message', TX_Message.b_6);
-        msg.setByteInTelegram(CONSTANTS.START, CONSTANTS.STX);
-        msg.setUint16(0, TX_Message.b_0);
-        msg.setUint16(1, TX_Message.b_1);
-        msg.setUint16(2, TX_Message.b_2);
-        msg.setUint16(3, TX_Message.b_3);
-        msg.setUint16(4, TX_Message.b_4);
-        msg.setUint16(5, TX_Message.b_5);
-        msg.setUint16(6, TX_Message.b_6);
-        msg.setUint16(7, TX_Message.b_7);
-        msg.setUint16(8, TX_Message.b_8);
-        msg.setUint16(9, TX_Message.b_9);
-        msg.setByteInTelegram(CONSTANTS.STOP, CONSTANTS.ETX);
-        msg.encodeTelegram();
-        console.log(msg.getBuffer().toString('ascii'));
-        await port.send(msg.getBuffer().toString('ascii'));
-      } catch (e) {
-        console.log(e);
-      }
-    }, interval);
-  }
-};
+  }, interval);
+}
 
 init();
-*/
+
 //start server
 server([
   get('/', async ctx => await render('./public/index.html')),
