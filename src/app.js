aca84a432f374a87d07947df95a4fa7eaea94826
Pavol Risa <risapav@gmail.com>
2019-07-17 19:33:27 +0200

    statis server

--- src/app.js
+++ src/app.js
@@ -295,7 +295,7 @@ server({
 }, [
   //ctx => { throw new Error('I am an error!'); },
   get('/', ctx => render('index.hbs')), // eslint-disable-line no-unused-vars
-  get('js/main.js', ctx => file('js/main.js')), 
+  get('/js/main.js', ctx => file('js/main.js')), 
   get('/rx', ctx => json(rx_msg)), // eslint-disable-line no-unused-vars
   post('/tx', ctx => {
     //nastav prislusny bit v 16 bit commande
